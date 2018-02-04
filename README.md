0e2aee64d6561dc634997172fbd68945275d781a
Przemysław Ostrowiński <p.ostrowinski@gmail.com>
2018-02-04 01:09:47 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-It's basic template with used:
-- Sass
-- Grunt
-- Bootstrap
+Script adding margin and padding by using specified class names for example
+
+pl-30 => padding-left: 30px;
+mb-100 => margin-bottom: 100px;
--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
-const elementsDOM = firstLetter => {
-    const arrElements = [...document.querySelectorAll(`[class^=${firstLetter}]`)];
-    const arrDirectionExtension = arrElements.map(classElement => {
-        const boxModelProp = paddingOrMargin(classElement.className[0]);
-        const direction = cssDirections(classElement.className[1]);
-        const pixels = classElement.className.substr(3);
-        const propAndDirection = `${boxModelProp}${direction}`;
-        return classElement.style.setProperty(propAndDirection, `${pixels}px`)
+'use strict';
+
+var setMarginOrPaddingOnElementsDOM = function elementsDOM(firstLetter) {
+    var elementsNodeList = document.querySelectorAll('[class^=' + firstLetter);
+    var arrElements = Array.prototype.slice.call(elementsNodeList);
+    var filterArrElements = arrElements.filter(function (item) {
+        return item.className[2] === '-';
     });
-    
-    function paddingOrMargin (prop) {
-        const props = {
-            'm' : 'margin',
-            'p' : 'padding',
-        }
+    var completeStyleForPaddingAndMargin = filterArrElements.map(function (classElement) {
+        var boxModelProp = paddingOrMargin(classElement.className[0]);
+        var direction = cssDirections(classElement.className[1]);
+        var pixels = classElement.className.substr(3);
+        var propAndDirection = '' + boxModelProp + direction;
+        return classElement.style.setProperty(propAndDirection, pixels + 'px');
+    });
+
+    function paddingOrMargin(prop) {
+        var props = {
+            'm': 'margin',
+            'p': 'padding'
+        };
         return props[prop];
     }
-    
-    function cssDirections (direction) {
-        const directions = {
-            't' : '-top',
-            'b' : '-bottom',
-            'l' : '-left',
-            'r' : '-right',
-            'a' : '' 
-        }
+
+    function cssDirections(direction) {
+        var directions = {
+            't': '-top',
+            'b': '-bottom',
+            'l': '-left',
+            'r': '-right',
+            'a': ''
+        };
         return directions[direction];
     }
-    return arrDirectionExtension;
-}
+    return completeStyleForPaddingAndMargin;
+};
 
-elementsDOM('p');
-elementsDOM('m');
+setMarginOrPaddingOnElementsDOM('p');
+setMarginOrPaddingOnElementsDOM('m');
